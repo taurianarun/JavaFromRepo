a04d4e4efb047395fc465e240aebaffdb5cc20cd
taurianarun <107833420+taurianarun@users.noreply.github.com>
2022-06-21 15:46:57 +0530

    Update JavaProg.java

--- JavaProg.java
+++ JavaProg.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] s) {
-        System.out.println("Hello Oracle");
+        System.out.println("Hello Oracle Again");
         for(int i = 0; i<=10; i++) {
             System.out.println("Value of I : " + i);
         }
